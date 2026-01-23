d41598985303e507111a3772a80f1c0626bf0757
azmilazizi <58665721+azmilazizi@users.noreply.github.com>
2026-01-23 16:20:38 +0800

    Fix analytics card footer border

--- modules/AppAnalytics/resources/views/index.blade.php
+++ modules/AppAnalytics/resources/views/index.blade.php
@@ -35,30 +35,39 @@
                         @foreach($group['accounts'] as $account)
                             <div class="col-12 col-sm-6 col-lg-4 col-xxl-3">
                                 <div class="card shadow-none border-gray-300 h-100 analytics-card analytics-channel-card">
-                                    <div class="card-body d-flex align-items-center gap-16">
-                                        <div class="text-gray-600 analytics-channel-avatar d-flex align-items-center justify-content-between position-relative">
-                                            <img
-                                                data-src="{{ Media::url($account->avatar) }}"
-                                                src="{{ theme_public_asset('img/default.png') }}"
-                                                class="b-r-100 w-full h-full border-1 lazyload"
-                                                onerror="this.src='{{ theme_public_asset('img/default.png') }}'"
-                                            >
-                                            @if($account->module_item)
-                                                <span class="size-16 border-1 b-r-100 position-absolute fs-9 d-flex align-items-center justify-content-between text-center text-white b-0 r-0 analytics-channel-badge" style="background-color: {{ $account->module_item['color'] ?? '#6f58ff' }};">
-                                                    <div class="w-100"><i class="{{ $account->module_item['icon'] ?? 'fa-light fa-chart-line' }}"></i></div>
-                                                </span>
-                                            @endif
-                                        </div>
-                                        <div class="flex-grow-1 fs-14 fw-5 text-truncate">
-                                            <div class="text-truncate text-gray-900">{{ $account->name }}</div>
-                                            <div class="fs-12 text-gray-600 text-truncate">
-                                                {{ __(ucfirst($account->social_network.' '.$account->category)) }}
+                                    <div class="card-body">
+                                        <div class="d-flex flex-grow-1 align-items-top gap-8">
+                                            <div class="text-gray-600 size-40 min-w-40 d-flex align-items-center justify-content-between position-relative">
+                                                <a href="{{ $account->url ?? '#' }}" target="_blank" class="text-gray-900 text-hover-primary">
+                                                    <img
+                                                        data-src="{{ Media::url($account->avatar) }}"
+                                                        src="{{ theme_public_asset('img/default.png') }}"
+                                                        class="b-r-100 w-full h-full border-1 lazyload"
+                                                        onerror="this.src='{{ theme_public_asset('img/default.png') }}'"
+                                                    >
+                                                </a>
+                                                @if($account->module_item)
+                                                    <span class="size-17 border-1 b-r-100 position-absolute fs-9 d-flex align-items-center justify-content-between text-center text-white b-0 r-0" style="background-color: {{ $account->module_item['color'] ?? '#6f58ff' }};">
+                                                        <div class="w-100"><i class="{{ $account->module_item['icon'] ?? 'fa-light fa-chart-line' }}"></i></div>
+                                                    </span>
+                                                @endif
+                                            </div>
+                                            <div class="flex-grow-1 fs-14 fw-5 text-truncate">
+                                                <div class="text-truncate">
+                                                    <a href="{{ $account->url ?? '#' }}" target="_blank" class="text-gray-900 text-hover-primary">
+                                                        {{ $account->name }}
+                                                    </a>
+                                                </div>
+                                                <div class="fs-12 text-gray-600 text-truncate">
+                                                    {{ __(ucfirst($account->social_network.' '.$account->category)) }}
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="card-footer fs-12 d-flex justify-content-center">
-                                        <a class="btn btn-sm analytics-channel-link" href="{{ route('app.analytics.show', ['platform' => strtolower($account->social_network), 'id' => $account->id_secure]) }}">
-                                            <i class="fa-light fa-chart-simple me-1"></i> {{ __('View') }}
+                                    <div class="card-footer fs-12">
+                                        <a href="{{ route('app.analytics.show', ['platform' => strtolower($account->social_network), 'id' => $account->id_secure]) }}" class="d-flex w-100 gap-8 align-items-center justify-content-center text-gray-900 text-hover-primary fw-5 py-2">
+                                            <i class="fa-light fa-chart-simple"></i>
+                                            <span>{{ __('View') }}</span>
                                         </a>
                                     </div>
                                 </div>
