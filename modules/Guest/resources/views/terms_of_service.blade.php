68c4665b82c1accd86d2cd9e4f27e9ea4a5b2596
azmilazizi <azmilazizi1999@graduate.utm.my>
2026-01-26 12:00:18 +0800

    latest

--- modules/Guest/resources/views/terms_of_service.blade.php
+++ modules/Guest/resources/views/terms_of_service.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('pagetitle', __("Terms of Service)
+@section('pagetitle', __("Terms of Service"))
 @section('content')
   @include('pages.terms_of_service')
 @endsection
